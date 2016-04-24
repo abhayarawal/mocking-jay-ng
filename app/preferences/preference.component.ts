36495e5846350a9d4c577b949e931ab525a68dd9
awesomegamer <avay_raul@hotmail.com>
2016-04-23 19:22:09 -0500

    preferences form layout

--- app/preferences/preference.component.ts
+++ app/preferences/preference.component.ts
@@ -12,7 +12,56 @@ import {NotificationService, Notification} from '../notification.service';
 
 import {AuthService} from '../auth/auth.service';
 import {UserService} from '../services/user.service';
- 
+import {RadiusInputComponent, RadiusSelectComponent, RadiusRadioComponent} from '../form/form.component';
+
+enum AlternateType {
+	Phone,
+	Email
+}
+
+interface Alternate {
+	data: string,
+	show: boolean,
+	notification: boolean,
+	activated: boolean,
+	type: AlternateType
+}
+
+
+@Component({
+	selector: 'alternate-pref',
+	template: `
+		<div class="email__pref" *ngIf="alternate">
+			<h5>
+				<span class="icon-mail" *ngIf="alternate.type==1"></span>
+				<span class="icon-phone" *ngIf="alternate.type==0"></span>
+				{{alternate.data}}
+			</h5>
+			<div *ngIf="!alternate.activated" class="activation">
+				<strong>Not activated yet.</strong>
+				<a>Send another activation code</a>
+				<div class="form__group">
+					<input type="text" placeholder="Activation code" />
+					<button>Activate</button>
+				</div>
+			</div>
+			<div class="ctls" *ngIf="alternate.activated">
+				<section>
+					<label>Show in contact card?</label>
+					<radius-radio [intext]="true" [on]="alternate.show"></radius-radio>
+				</section>
+				<section>
+					<label>Use for notification?</label>
+					<radius-radio [intext]="true" [on]="alternate.notification"></radius-radio>
+				</section>
+			</div>
+		</div>
+	`,
+	directives: [RadiusRadioComponent]
+})
+class AlternatePref {
+	@Input() alternate: Alternate;
+}
 
 
 @Component({
@@ -23,7 +72,26 @@ import {UserService} from '../services/user.service';
 				<span class="lnr lnr-pencil"></span>
 				Modify your preferences
 			</h4>
-			<div class="form__wrap">
+			<div class="form__wrap pref__form">
+				<form>
+					<div class="form__group">
+						<h5 class="form__title">Email preferences:</h5>
+						<label>Add a new email</label>
+						<input type="text" placeholder="john.doe@example.com" />
+					</div>
+					<div class="form__group">
+						<alternate-pref *ngFor="#ae of emails" [alternate]="ae"></alternate-pref>
+					</div>
+					
+					<div class="form__group">
+						<h5 class="form__title">Phone preferences:</h5>
+						<label>Add a new phone</label>
+						<input type="text" placeholder="000-000-0000" />
+					</div>
+					<div class="form__group">
+						<alternate-pref *ngFor="#ap of phones" [alternate]="ap"></alternate-pref>
+					</div>
+				</form>
 			</div>
 		</div>
 
@@ -34,17 +102,24 @@ import {UserService} from '../services/user.service';
 					<span *ngIf="!user.meta.avatar">AR</span>
 				</section>
 				<h3>{{user.lname}}, {{user.fname}}</h3>
-				<div class="email">
-					
-				</div>
+				<ul class="email">
+					<li>{{user.email}}</li>
+				</ul>
 			</div>
 		</div>
 	`,
-	directives: []
+	directives: [AlternatePref]
 })
 class PreferencesForm implements OnInit {
-
 	user: User;
+	emails: Alternate[] = [
+		{ data: 'fuckoff@smurf.com', show: true, notification: false, activated: true, type: AlternateType.Email },
+		{ data: 'katy.perry@music.com', show: true, notification: false, activated: false, type: AlternateType.Email },
+	];
+
+	phones: Alternate[] = [
+		{ data: '000-000-9999', show: true, notification: true, activated: true, type: AlternateType.Phone },
+	];
 
 	constructor(
 		private fb: FormBuilder,
